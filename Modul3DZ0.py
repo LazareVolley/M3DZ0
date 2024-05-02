0e23a0748e07c2c7f9b68f51ea22fe30c066decc
LazareVolley <LazareVolley@yandex.ru>
2024-05-03 01:08:17 +0300

    exercize 1

---
+++ Modul3DZ0.py
@@ -0,0 +1,15 @@
+Задача №1. Описание Домашних животных
+
+struct Animal:
+    name:  str,
+    type: str,
+    age: int,
+    weight: float,
+    vaccination: bool
+
+Например:
+
+Parrot = Animal("Kesha", "bird", 5, 1.2, True)
+Dolphin = Animal("", "fish", 24 ,243.11, False)
+Makaka = Animal("ChiChiChi", "Monkey", 10, 28.7, True)
+Gaduka = Animal("Djafar", "Snake", 1, 4.97, False)
